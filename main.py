f3e83c307aff80cf5c9f3a44a826cf0ee68461c3
Abdullah <irsalabd@gmail.com>
2019-04-14 12:28:29 +0600

    Changes in main()

--- main.py
+++ main.py
@@ -355,6 +355,24 @@ class Update(ConverterMixin):
         assert False, 'kek'
 
 
+def test(bot):
+
+    assert bot._prepare_value(dict(allowed_updates=[Update.Type.MESSAGE, Update.Type.EDITED_MESSAGE],
+                                   after=Update(id=1), chat=Chat(id=1, type='private'),
+                                   reply_to_message=Message(id=1, date=0, chat=dict(id=1, type='private'), text='lel kek xd'),
+                                   caption='lel kek xd', parse_mode=Message.ParseMode.MARKDOWN,
+                                   action=Chat.Action.TYPING,
+                                   some=None, another=None, kek=[1,2, None]
+                                   )) == dict(allowed_updates=['message', 'edited_message'],
+                                              after=2, chat=1,
+                                              reply_to_message=1,
+                                              caption='lel kek xd',
+                                              parse_mode='Markdown',
+                                              action='typing',
+                                              kek=[1,2]
+                                              )
+
+
 def main():
     BOT_API_TOKEN = config('BOT_API_TOKEN', cast=str)
     LOGLEVEL = config('LOGLEVEL', cast=str, default='INFO')
@@ -364,6 +382,8 @@ def main():
 
     bot = Bot.by(BOT_API_TOKEN)
 
+    test(bot)
+
     webhookinfo = bot.webhookinfo()
 
     updates = bot.updates()
@@ -372,8 +392,8 @@ def main():
         print(u)
         print(u.type)
 
-    # updates = bot.updates(after=locals().get('u'))
-    # assert updates == []
+    updates = bot.updates(after=locals().get('u'))
+    assert updates == []
 
     # action_sent = bot.send_chat_action(u.message.chat, Chat.Action.TYPING)
     # time.sleep(0.5)
@@ -381,14 +401,14 @@ def main():
     # sent_message = bot.send_message(chat=u.message.chat, text='*lel* _kek_ `xd`', parse_mode=Message.ParseMode.MARKDOWN)
     # print(sent_message)
 
-    # import io
-    # with io.BytesIO(b'some content file') as f: 
-    #     f.name = 'kek.txt'
+    # # import io
+    # # with io.BytesIO(b'some content file') as f: 
+    # #     f.name = 'kek.txt'
 
-    # message_with_doc = bot.send_document(chat=u.message.chat, document='BQADBAADmQAD17aEUaCF8A1RHZMnAg', 
+    # message_with_doc = bot.send_document(chat=u.message.chat, document='BQADAgAD4QIAAobqmUmE8Yh3E-vvMgI', 
     #                                      caption='hey see my _*document*_ here',
     #                                      parse_mode=Message.ParseMode.MARKDOWN,
-    #                                      reply_to_message_id=u.message.id,
+    #                                      reply_to_message=u.message,
     #                                      )
 
     # print(message_with_doc)
